9cccccdfccdf0d0780402fe6bd3dd38cb104746a
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-06-05 14:04:07 +0200

    Create life cycle components

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -5,4 +5,24 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './home-page.html',
 })
-export class HomePage {}
+export class HomePage {
+  constructor() {};
+
+ngOnInit(){
+  console.log(`ngOnInit: Runs once the component is initialized.`); }
+
+ngDoCheck(){
+  console.log(`ngDoCheck: Runs every time this component is checked for changes.`); }
+
+ngAfterContentInit(){
+  console.log(`ngAfterContentInit: Runs once after the component's content has been initialized.`); }
+
+ngAfterContentChecked(){
+  console.log(`ngAfterContentChecked: Runs every time this component content has been checked for changes.`); }
+
+ngAfterViewInit(){
+  console.log(`ngAfterViewInit: Runs once after the component's view has been initialized.`); }
+
+ngAfterViewChecked(){
+  console.log(`ngAfterViewChecked: Runs every time the component's view has been checked for changes.`); }
+}
